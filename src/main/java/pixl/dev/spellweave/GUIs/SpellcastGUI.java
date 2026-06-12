c904ebb7618e3a972d25873da85dc44a76d2ce22
pixl <pixl.dev@gmail.com>
2026-06-12 18:22:38 -0400

    Added GUI items

---
+++ src/main/java/pixl/dev/spellweave/GUIs/SpellcastGUI.java
@@ -0,0 +1,47 @@
+package pixl.dev.spellweave.GUIs;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class SpellcastGUI {
+    public void openSpellcastGUI(Player p) {
+        Inventory inv = Bukkit.createInventory(p, 9*5, "Spellcasting");
+
+        // Filler item
+        ItemStack filler = new ItemStack(Material.GRAY_STAINED_GLASS_PANE);
+        ItemMeta fillerMeta = filler.getItemMeta();
+        fillerMeta.setDisplayName(null);
+        filler.setItemMeta(fillerMeta);
+
+        // Exit button
+        ItemStack exit = new ItemStack(Material.BARRIER);
+        ItemMeta exitMeta = exit.getItemMeta();
+        exitMeta.setDisplayName(ChatColor.RED + "Exit");
+        exit.setItemMeta(exitMeta);
+
+        // Confirm button
+        ItemStack confirm = new ItemStack(Material.EMERALD);
+        ItemMeta confirmMeta = confirm.getItemMeta();
+        confirmMeta.setDisplayName(ChatColor.GREEN + "Confirm Cast");
+        confirm.setItemMeta(confirmMeta);
+
+        // Spellcast picture buttons (clicked / red glass and not clicked / green glass)
+
+            // Clicked
+        ItemStack clicked = new ItemStack(Material.RED_STAINED_GLASS_PANE);
+        ItemMeta clickedMeta = clicked.getItemMeta();
+        clickedMeta.setDisplayName(null);
+        clicked.setItemMeta(clickedMeta);
+
+            // Not clicked
+        ItemStack notClicked = new ItemStack(Material.LIME_STAINED_GLASS_PANE);
+        ItemMeta notClickedMeta = notClicked.getItemMeta();
+        notClickedMeta.setDisplayName(null);
+        notClicked.setItemMeta(notClickedMeta);
+    }
+}
