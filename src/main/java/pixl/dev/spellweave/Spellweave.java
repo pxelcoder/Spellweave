9b911cb30faa934c1743e211d5cca56013d578dc
Hyacinthvs <hyacinthusyt@gmail.com>
2026-06-11 21:55:28 -0400

    Initial commit

---
+++ src/main/java/pixl/dev/spellweave/Spellweave.java
@@ -0,0 +1,16 @@
+package pixl.dev.spellweave;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Spellweave extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
